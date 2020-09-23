a1fcf5ca56eb7b46b7bdee4caef456ea08d2fc33
Bassem Abbas <Bassem.torky@thelucky.app>
2020-09-23 17:31:22 +0200

    - fix compiler error 'unable to type-check this expression in reasonable time' - update example to swift 5

--- CountdownLabel/LTMorphingLabel/LTEasing.swift
+++ CountdownLabel/LTMorphingLabel/LTEasing.swift
@@ -24,7 +24,7 @@ public struct LTEasing {
     
     public static func easeInQuint(_ t: Float, _ b: Float, _ c: Float, _ d: Float = 1.0) -> Float {
         return {
-            return c * $0 * $0 * $0 * $0 * $0 + b
+            return (c * ($0 * $0 * $0 * $0 * $0)) + b
             }(t / d)
     }
     
@@ -37,16 +37,16 @@ public struct LTEasing {
     public static func easeOutBounce(_ t: Float, _ b: Float, _ c: Float, _ d: Float = 1.0) -> Float {
         return {
             if $0 < 1 / 2.75 {
-                return c * 7.5625 * $0 * $0 + b
+                return (c * (7.5625 * $0 * $0)) + b
             } else if $0 < 2 / 2.75 {
                 let t = $0 - 1.5 / 2.75
-                return c * (7.5625 * t * t + 0.75) + b
+                return (c * (7.5625 * t * t + 0.75)) + b
             } else if $0 < 2.5 / 2.75 {
                 let t = $0 - 2.25 / 2.75
-                return c * (7.5625 * t * t + 0.9375) + b
+                return (c * (7.5625 * t * t + 0.9375)) + b
             } else {
                 let t = $0 - 2.625 / 2.75
-                return c * (7.5625 * t * t + 0.984375) + b
+                return (c * (7.5625 * t * t + 0.984375)) + b
             }
         }(t / d)
     }
